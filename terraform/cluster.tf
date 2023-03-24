63a629ed6fa781f35da6bb01b0381cdbb71949e7
hugo4715 <deniauhugo@gmail.com>
2023-03-24 11:09:58 +0100

    add k8s cluster

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,3 +1,9 @@
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_container_cluster" "primary" {
+  name               = "mc-cluster"
+  initial_node_count = 3
+ 
+  timeouts {
+    create = "15m"
+    update = "15m"
+  }
 }
