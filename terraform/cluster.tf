cb79a0b8c7f8a4667f22e57c0b89a7e356d480ba
hugo4715 <deniauhugo@gmail.com>
2023-03-24 10:57:06 +0100

    removed service account

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,19 +1,8 @@
-resource "google_service_account" "default" {
-  account_id   = "service-account-1"
-  display_name = "Service Account 1"
-}
-
 resource "google_container_cluster" "primary" {
   name               = "mc-cluster"
   location           = "eu-central1-c"
   initial_node_count = 3
-  node_config {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+ 
   timeouts {
     create = "15m"
     update = "15m"
