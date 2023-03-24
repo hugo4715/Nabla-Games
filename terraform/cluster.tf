4a9e159c52226f7f04e54eba7b5463fc72a20740
hugo4715 <deniauhugo@gmail.com>
2023-03-24 11:02:39 +0100

    Removed everything

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,9 +1,3 @@
-resource "google_container_cluster" "primary" {
-  name               = "mc-cluster"
-  initial_node_count = 3
- 
-  timeouts {
-    create = "15m"
-    update = "15m"
-  }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
